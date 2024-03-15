97dcaa0a70eee7e8b9e66cef053abaf218de3f95
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-15 21:06:04 +0100

    UPDATE - Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,11 @@
 import Link from "next/link"
-import FlagIcon from "../FlagIcon/FlagIcon"
 
 export default function Header() {
   return (
     <header className="flex items-center py-6">
       <div className="container flex items-center gap-4">
         <Link className="flex items-center gap-2" href="#">
-          <FlagIcon className="h-6 w-6" />
-          <span className="text-lg font-semibold">Electrician</span>
+          <span className="text-lg font-semibold">Cirima</span>
         </Link>
         <nav className="ml-auto flex items-center gap-4 text-sm font-medium">
           <Link className="text-gray-900 dark:text-gray-100 px-2.5 py-2 rounded-md" href="#">
