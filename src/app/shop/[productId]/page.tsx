cca41e81b7cf1668faf710927a0bc29bfb1d7b18
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-25 14:54:46 +0100

    UPDATE - Product page styles

--- src/app/shop/[productId]/page.tsx
+++ src/app/shop/[productId]/page.tsx
@@ -1,9 +1,10 @@
 import Header from "@/components/Header/Header";
 import Image from "next/image";
 import { products } from "../items"
-import image from '@/../public/instal-electricas.jpg'
+import image from '@/../public/rodamiento.jpeg'
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { PhoneIcon } from "@/components/Icons/Icons";
 
 interface Props {
   params: {
@@ -19,18 +20,19 @@ export default function page({ params }: Props) {
           <Image src={image} alt={product.name} className="h-full rounded-lg w-full object-cover" />
         </div>
         <div className="py-4">
-          <h2 className="text-2xl font-bold mb-2">Product Name</h2>
-          <p className="text-gray-500 mb-4">Product Description</p>
-          <p className="text-lg font-bold mb-4">$99.99</p>
-          <h3 className="text-lg font-bold mb-2">Specifications</h3>
-          <ul className="list-disc py-4 list-inside space-y-1">
-            <li>Specification 1</li>
-            <li>Specification 2</li>
-            <li>Specification 3</li>
+          <h2 className="text-3xl font-bold mb-2">Rodamiento</h2>
+          <p className="text-gray-500 mb-4">Rodamiento de lavadora modelo LG-567  </p>
+          <p className="text-lg font-bold mb-4">45€</p>
+          <h3 className="text-lg font-bold mb-2">Especificaciones</h3>
+          <ul className="list-disc text-gray-700 py-4 list-inside space-y-1">
+            <li>Lavadoras LG</li>
+            <li>Ano 2003</li>
+            <li>Diametro: 10 cm</li>
           </ul>
           <Link
-            className="inline-flex text-gray-50 h-10 md:w-40 place-self-center items-center justify-center rounded-md border  border-gray-200 bg-blue-500 px-8 text-sm font-medium shadow-sm transition-colors hover:bg-blue-600 hover:text-gray-50 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-blue-800 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+            className=" text-gray-50 flex gap-2 h-10 md:w-40 place-self-center items-center justify-center rounded-md border  border-gray-200 bg-blue-500 px-8 text-sm font-medium shadow-sm transition-colors hover:bg-blue-600 hover:text-gray-50 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-blue-800 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
             href="#">
+            <PhoneIcon />
             Reservalo
           </Link>
         </div>
