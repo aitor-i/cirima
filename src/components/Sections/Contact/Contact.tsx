6c98a980ac110cc1f6a5d25f8b1066bc36a7b2cb
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-25 10:01:38 +0100

    REFATOR - Contact

---
+++ src/components/Sections/Contact/Contact.tsx
@@ -0,0 +1,13 @@
+import ContactForm from '@/components/ContactForm/ContactForm'
+import ContactInfo from '@/components/ContactInfo/ContactInfo'
+
+export default function Contact() {
+  return (
+    <section className="grid items-center grid-cols-1 md:grid-cols-2 gap-6">
+      <div className="container max-w-lg">
+        <ContactForm />
+      </div>
+      <ContactInfo />
+    </section>
+  )
+}
