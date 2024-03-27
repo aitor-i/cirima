da6d9494788bec9d2401f14fd5f42298010d45dc
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-27 09:54:41 +0100

    ADD - Email template

---
+++ src/components/EmailTemplate/EmailTemplate.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+interface Props {
+
+  name: string,
+  surname: string
+  email: string
+  message: string
+}
+
+export default function EmailTemplate({ name, surname, email, message }: Props) {
+  return (
+    <div className='py-20  h-screen px-40 flex w-screen flex-col items-center'>
+      <span className='h-fit  '>
+        <h2 className='text-2xl font-bold'>{name} {" "} {surname} ha enviado un mensaje</h2>
+        <p>{message}</p>
+        <i>{email}</i>
+      </span>
+    </div>
+  )
+}
