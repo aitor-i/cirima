83ad120d83c9c040535f2f34a0496fa1799fb5f3
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-27 09:54:55 +0100

    ADD - Contact form email resend

--- src/app/api/contact-form/route.ts
+++ src/app/api/contact-form/route.ts
@@ -1,4 +1,8 @@
+import EmailTemplate from '@/components/EmailTemplate/EmailTemplate';
 import { NextRequest, NextResponse } from 'next/server';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const dynamic = 'force-dynamic' // defaults to auto
 
@@ -6,8 +10,18 @@ export async function POST(req: NextRequest) {
   // Perform your logic here
   const body = await req.json();
 
-  console.log('Body: ', body)
+  const { name, surname, email, message } = body
+
+  const { data, error } = await resend.emails.send({
+    from: 'Contact <contact@steel-mountain.dev>',
+    to: ['contact@aitoribarra.com'],
+    subject: `${name} - Ha enviado un mensaje atraves de la web!`,
+    react: EmailTemplate({ name, message, surname, email }),
+  });
+
   const response = { message: "Message saved!", body };
+
+  console.log(data, error)
   return new NextResponse(JSON.stringify(response), {
     status: 200,
     headers: {
