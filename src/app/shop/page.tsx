fc8c6c261449580228fe46680715e51f4b6ef1a1
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-29 18:49:54 +0100

    ADD - Product fetching from db

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,7 +1,15 @@
 import ProductCard from "./ProductCard"
-import { products } from "./items"
-import image from '@/../public/rodamiento.jpeg'
-export default function page() {
+import { sql } from "@vercel/postgres";
+export default async function page() {
+  async function getProducts() {
+    'use server'
+    const products = await sql`SELECT * FROM products`;
+    return products.rows
+  }
+
+  const products = await getProducts()
+  console.log(products)
+
   return (
     <div className="grid grid-rows-[auto 1fr auto] gap-6 min-h-screen px-4 sm:px-6 lg:px-8">
       <main className="grid  gap-12 py-8">
@@ -11,11 +19,12 @@ export default function page() {
         </section>
         <section className="grid grid-cols-1  gap-4 md:grid-cols-2 md:gap-6 lg:grid-cols-3">
           {products.map(product => <ProductCard
+            id={product.id}
             key={product.name}
             description={product.description}
             price={product.price.toString()}
             title={product.name}
-            image={image} />)}
+            image={product.imagepath} />)}
         </section>
 
 
