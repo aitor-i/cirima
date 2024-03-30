4cd0a53553cbf5c757ec581422e1ec2ef55df89a
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-30 17:09:48 +0100

    ADD - Product not found page

---
+++ src/app/shop/[productId]/not-found.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+
+export default function page() {
+  return (
+    <section className="flex flex-col py-32 items-center  h-screen  ">
+      <div className="text-center space-y-4 flex flex-col gap-2">
+        <h1 className="text-6xl font-bold text-gray-900 ">404 Not Found</h1>
+        <p className="text-xl text-gray-600">Oops! La pagina que buscas no existe</p>
+        <Link
+          className=""
+          href="/shop"
+        >
+          <Button className='bg-blue-500 hover:bg-blue-600'> Volver a la tienda</Button>
+        </Link>
+      </div>
+    </section>
+  )
+}
