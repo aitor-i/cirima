f069a1f1b54de8b82540ffc9f0d1352eab5cbf0e
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-31 13:44:09 +0200

    FIX - Product page

--- src/app/shop/[productId]/page.tsx
+++ src/app/shop/[productId]/page.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { PhoneIcon } from "@/components/Icons/Icons";
 import { getProduct } from "@/serverActions/products";
-import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 interface Props {
@@ -37,7 +36,7 @@ export default async function page({ params }: Props) {
           <p className="text-lg font-bold mb-4">{product.price}</p>
           <h3 className="text-lg font-bold mb-2">Especificaciones</h3>
           <ul className="list-disc text-gray-700 py-4 list-inside space-y-1">
-            {product.specifications}
+            {product.spec}
           </ul>
           <Link
             className=" text-gray-50 flex gap-2 h-10 md:w-40 place-self-center items-center justify-center rounded-md border  border-gray-200 bg-blue-500 px-8 text-sm font-medium shadow-sm transition-colors hover:bg-blue-600 hover:text-gray-50 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-blue-800 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
